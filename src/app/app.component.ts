57e27dc8ef4c873bf224df24216801a0a911c183
Klaudia Moroń <klaudia.moron@jsjgroup.pl>
2018-10-17 08:20:58 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './interfaces/todo-item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tour-heroes';
+  title = 'My To Do List APP';
 }
